3ef8d6efc11bdd80b531c18aab9f4cb438ce2ec0
Kuleb9lka <winkeylinks@gmail.com>
2026-02-21 11:16:02 +0300

    init commit

---
+++ src/main/java/com/ITQGroup/ItqGroupApplication.java
@@ -0,0 +1,13 @@
+package com.ITQGroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItqGroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItqGroupApplication.class, args);
+	}
+
+}
