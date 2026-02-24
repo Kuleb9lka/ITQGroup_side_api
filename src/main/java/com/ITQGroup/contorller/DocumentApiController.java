cc855e503c33948367ee474a48a22c66b0347f01
Kuleb9lka <winkeylinks@gmail.com>
2026-02-24 09:42:44 +0300

    added api swagger documentation implementation

--- src/main/java/com/ITQGroup/contorller/DocumentApiController.java
+++ src/main/java/com/ITQGroup/contorller/DocumentApiController.java
@@ -1,5 +1,6 @@
 package com.ITQGroup.contorller;
 
+import com.ITQGroup.api.DocumentApi;
 import com.ITQGroup.dto.DocumentConcurrentRequestDto;
 import com.ITQGroup.dto.DocumentConcurrentResponseDto;
 import com.ITQGroup.dto.DocumentShortResponseDto;
@@ -17,7 +18,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/documents-api")
 @RequiredArgsConstructor
-public class DocumentApiController {
+public class DocumentApiController implements DocumentApi {
 
     private final RequestRunnerService requestRunnerService;
 
